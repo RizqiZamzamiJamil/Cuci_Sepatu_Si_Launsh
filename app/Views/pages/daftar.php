bb4cda2debcd30e2f2ea7af90f5638bc96131727
RizqiZamzamiJamil <rizqizamzamij@gmail.com>
2023-05-17 21:26:48 +0700

    Perubahan1 Daftar

--- app/Views/pages/daftar.php
+++ app/Views/pages/daftar.php
@@ -18,7 +18,7 @@
 
     .image-container .row {
         position: relative;
-        width: 790px;
+        width: 1000px;
         color: white;
         margin-left: 110px;
     }
@@ -85,6 +85,52 @@
         left: 1187px;
     }
 
+    .form-label {
+        display: block;
+        font-weight: bold;
+    }
+
+    .form-input2 {
+        background: none;
+        width: 350px;
+        height: 42px;
+        border: 3px solid white;
+        border-radius: 10px;
+    }
+
+
+    .form-group input {
+        width: 48%;
+        padding: 5px;
+    }
+
+    .form-group .form-input-group {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .checkbox-group {
+        margin-bottom: 15px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .checkbox-group label {
+        font-weight: normal;
+    }
+
+    .submit-btn {
+        width: 200px;
+        border-radius: 10px;
+        padding: 10px;
+        background-color: white;
+        color: black;
+        border: none;
+        text-align: center;
+        cursor: pointer;
+    }
+
     footer {
         margin-top: -7px;
     }
@@ -104,11 +150,49 @@
                     <h1><b>Selamat Bergabung</b></h1>
                     <h3>>>>>>>></h2>
                 </div>
-                <form>
+                <!-- <div class="container-form">
+                    <form action="proses_register.php" method="POST">
+                        <div class="form-group">
+                            <label for="first-name">Nama Depan:</label>
+                            <div class="form-input-group">
+                                <input type="text" id="first-name" name="first_name" required>
+                                <input type="text" id="last-name" name="last_name" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Alamat Email:</label>
+                            <input type="email" id="email" name="email" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="phone">Nomor Telepon:</label>
+                            <input type="tel" id="phone" name="phone" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Kata Sandi:</label>
+                            <input type="password" id="password" name="password" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="confirm-password">Konfirmasi Kata Sandi:</label>
+                            <input type="password" id="confirm-password" name="confirm_password" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="address">Alamat:</label>
+                            <textarea id="address" name="address" rows="4" required></textarea>
+                        </div>
+                        <div class="checkbox-group">
+                            <label>
+                                <input type="checkbox" name="agree_policy" required>
+                                Setuju dengan kebijakan kami
+                            </label>
+                        </div>
+                        <button type="submit" class="submit-btn">Daftar</button>
+                    </form> -->
+
+
+                <!-- <form>
                     <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                        <label for="nama-depan" class="form-label">Nama Depan</label>
+                        <input type="username" class="form-control" id="username">
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Password</label>
@@ -119,8 +203,65 @@
                         <label class="form-check-label" for="exampleCheck1">Check me out</label>
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
+                </form> -->
             </div>
+            <!-- Baris 1 -->
+            <div class="row">
+                <div class="col">
+                    <label for="nama-depan" class="form-label">Nama Depan</label>
+                    <input type="text" id="nama-depan" class="form-input2" name="nama-depan" required>
+                </div>
+                <div class="col">
+                    <label for="nama-belakang" class="form-label">Nama Belakang</label>
+                    <input type="text" id="nama-belakang" class="form-input2" name="nama-belakang" required>
+                </div>
+            </div>
+            <br>
+            <!-- Baris 2 -->
+            <div class="row">
+                <div class="col">
+                    <label for="alamat-surel" class="form-label">Alamat surel</label>
+                    <input type="email" id="email" class="form-input2" name="email" required>
+                </div>
+                <div class="col">
+                    <label for="no-telp" class="form-label">No. Telepon</label>
+                    <input type="tel" id="phone" class="form-input2" name="phone" required>
+                </div>
+            </div>
+            <br>
+            <!-- Baris 3 -->
+            <div class="row">
+                <div class="col">
+                    <label for="kata-sandi" class="form-label">Kata Sandi</label>
+                    <input type="password" id="password" class="form-input2" name="password" required>
+                </div>
+                <div class="col">
+                    <label for="konfirm-kata-sandi" class="form-label">Konfirmasi Kata Sandi</label>
+                    <input type="password" id="confirm-password" class="form-input2" name="confirm-password" required>
+                </div>
+            </div>
+            <br>
+            <!-- Baris 4 -->
+            <div class="row">
+                <div class="col">
+                    <label for="alamat" class="form-label">Alamat</label>
+                    <input type="text" id="alamat" class="form-input2" name="alamat" required>
+                </div>
+            </div>
+            <br>
+            <!-- Baris 5 -->
+            <div class="row">
+                <div class="col">
+                    <div class="checkbox-group">
+                        <label>
+                            <input type="checkbox" name="agree_policy" required>
+                            Setuju Kebijakan Si Launsh
+                        </label>
+                    </div>
+                    <button type="submit" class="submit-btn">Daftar</button>
+                </div>
+            </div>
+
         </div>
     </div>
 </section>
