f3232995b97350ba5cb68c389a1fca9ae60db902
RizqiZamzamiJamil <rizqizamzamij@gmail.com>
2023-05-19 14:18:29 +0700

    Perubahan3-Navbar, home

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,3 +1,7 @@
+<?php
+$current_page = basename($_SERVER['PHP_SELF']);
+?>
+
 <style>
     .navbar {
         box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
@@ -8,7 +12,7 @@
     }
 
     .navbar-nav {
-    width: 1000px;
+        width: 1000px;
     }
 
     .navbar a:hover,
@@ -56,23 +60,31 @@
         color: #fff;
         background: #0FC598;
     }
+
+    a.nav-link.active {
+        font-weight: bold;
+    }
 </style>
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
     <div class="container">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="/">
             <img src="/img/logo.png" alt="Logo" class="logo">
             SI LAUNSH
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+
+
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="/">Beranda</a>
-                <a class="nav-link" href="/pages/about">Tentang Kami</a>
-                <a class="nav-link" href="/pages/testimoni">Testimoni</a>
-                <a class="nav-link" href="#">Hubungi Kami</a>
+            <div class="navbar-nav ms-auto py-4 py-lg-0">
+                <a class="nav-link <?php echo ($current_page == 'home.php') ? 'active' : ''; ?>" aria-current="page" href="/">Beranda</a>
+                <a class="nav-link <?php echo ($current_page == 'about.php') ? 'active' : ''; ?>" aria-current="page" href="/pages/about">Tentang Kami</a>
+<!--                 <a class="nav-link" aria-current="page" href="/">Beranda</a> -->
+<!--                 <a class="nav-link" href="/pages/about">Tentang Kami</a> -->
+                <a class="nav-link active" href="/pages/testimoni">Testimoni</a>
+                <a class="nav-link active" href="#">Hubungi Kami</a>
                 <a class="signup" href="/pages/daftar">Daftar</a>
                 <a class="signin" href="/pages/masuk">Masuk</a>
             </div>
--- app/Views/pages/home copy.php
+++ app/Views/pages/home copy.php
@@ -6,51 +6,21 @@
     /* Section-1 */
 
     .section-1 .container {
-        position: relative;
         width: 100%;
         height: 711px;
         margin-top: -80px;
 
         background: linear-gradient(136.6deg, #0FC598 21.76%, #BFDB38 146.07%);
-
-        background-size: cover;
-        /* Menyesuaikan ukuran gambar dengan latar belakang */
-        background-repeat: no-repeat;
-        /* Tidak mengulang gambar latar belakang */
-        background-position: center center;
-        /* Posisi gambar latar belakang di tengah-tengah */
         box-shadow: -9px 12px 38px rgba(0, 0, 0, 0.25);
         border-radius: 0px 0px 0px 380px;
         color: white;
-
     }
-
-    .section-1 .container::after {
-        content: "";
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-image: url('/img/hero-bg.png');
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-position: center center;
-        border-radius: 0px 0px 0px 380px;
-        /* Atur opasitas sesuai kebutuhan */
-    }
-
-
-
-
 
     .section-1 .col {
         margin-top: 200px;
         margin-left: 300px;
-    }
+        position: absolute;
 
-    .section-1 .row {
-        position: relative;
     }
 
     .section-1 .row h3 {
@@ -81,12 +51,12 @@
 
     .image-container img {
         position: absolute;
-        height: auto;
-        max-width: 100%;
-        margin-left: auto;
-        margin-right: auto;
         top: 0;
         left: 0;
+    }
+
+    .image-bg {
+        position: relative;
     }
 
     #hero {
@@ -95,13 +65,13 @@
         margin-left: -12px;
         border-radius: 0px 0px 0px 380px;
 
-
     }
 
     #hero-helm {
         width: 190px;
-        /*         height: 280px; */
+        height: 280px;
         margin-top: 120px;
+        /*         margin-left: 1187px; */
         margin-left: 1120px;
     }
 
@@ -250,31 +220,6 @@
         margin-left: 70px;
     }
 
-    .section-3 .row.text-center {
-        margin-top: 60px;
-        text-align: center !important;
-        justify-content: center;
-    }
-
-    .section-3 .col-md-4 {
-        background-color: white;
-        justify-content: center;
-        align-content: center;
-        margin: 20px;
-        width: 300px;
-        border-radius: 20px;
-        height: 450px;
-    }
-
-    .section-3 .col-md-4 p {
-        font-size: 24px;
-    }
-
-    .section-3 h3 {
-        color: #000;
-        font-weight: bold;
-    }
-
     #pelayanan {
         position: absolute;
         height: 100%;
@@ -282,54 +227,12 @@
         margin-left: -12px;
         border-radius: 0px 380px 0px 0px;
     }
-
-    /* Section 4 */
-    .section-4 {
-        margin-top: 70px;
-    }
-
-    .section-4 h1 {
-        font-size: 50px;
-    }
-
-    .section-4 p {
-        font-size: 30px;
-        margin-bottom: 100px;
-    }
-
-    .section-4 .row.text-center .col {
-        background: none;
-    }
-
-    .section-4 .row {}
-
-    .section-4 .row .col {
-        margin: 0px 10px 0px;
-    }
-
-    .section-4 img {
-        width: 100%;
-    }
-
-    /* Section 5 */
-    .section-5 {
-        margin-top: 120px;
-
-    }
-    .section-5 .container{
-        width: 100%;
-        height: 711px;
-
-        background: linear-gradient(136.6deg, #0FC598 21.76%, #BFDB38 146.07%);
-        box-shadow: -18px -24px 38px rgba(0, 0, 0, 0.25);
-        border-radius: 380px 0px 0px 0px;
-    }
 </style>
 
 <section class="section-1">
     <div class="container">
         <div class="image-container">
-            <!--             <img src="/img/hero-bg.png" alt="" id="hero"> -->
+            <img src="/img/hero-bg.png" alt="" id="hero">
             <img src="/img/hero-helm.png" alt="" id="hero-helm">
             <img src="/img/hero-sepatu.png" alt="" id="hero-sepatu">
             <img src="/img/hero-bulat.png" alt="" id="hero-bulat">
@@ -391,68 +294,6 @@
                 <p>Kami Selalu Mengedepankan Pelayanan Terbaik Untuk Pelanggan</p>
             </div>
         </div>
-
-        <div class="row text-center">
-            <div class="col-md-4">
-                <h3 class="my-3">
-                    <img src="/img/pelayanan-jam.png" alt="">
-                    Pengerjaan Cepat
-                </h3>
-                <p class="text-muted">Dalam Pengerjaan layanan kepada pelanggan, kami selalu berusaha untuk mengerjakan sesuai dengan apa yang telah di tentukan</p>
-            </div>
-            <div class="col-md-4">
-                <h3 class="my-3">
-                    <img src="/img/pelayanan-like.png" alt="">
-                    Pengerjaan Rapi & Bersih
-                </h3>
-                <p class="text-muted">Selain Cepat tentu kami juga akan selalu mengawasi Tingkat Kebersihan dan Kerapian Sebelum Pelanggan Menerima barang tersebut</p>
-            </div>
-            <div class="col-md-4">
-                <h3 class="my-3">
-                    <img src="/img/pelayanan-smile.png" alt="">
-                    Menerima Kritik & Saran
-                </h3>
-                <p class="text-muted">Kami Selalu menerima Kritik & Saran dari Pelanggan demi kemajuan pelayanan kami kedepannya</p>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section class="section-4">
-    <div class="container">
-        <div class="row text-center">
-            <div class="col">
-                <h1><b>CABANG SI LAUNSH</b></h1>
-                <p>Kami Hadir Di Beberapa Kota Di Jawa Timur</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <img src="/img/cabang-surabaya.png" alt="">
-            </div>
-
-            <div class="col">
-                <img src="/img/cabang-malang.png" alt="">
-            </div>
-
-            <div class="col">
-                <img src="/img/cabang-pasuruan.png" alt="">
-            </div>
-
-            <div class="col">
-                <img src="/img/cabang-mojokerto.png" alt="">
-            </div>
-
-            <div class="col">
-                <img src="/img/cabang-sidoarjo.png" alt="">
-            </div>
-        </div>
-    </div>
-</section>
-
-<section class="section-5">
-    <div class="container">
-
     </div>
 </section>
 
