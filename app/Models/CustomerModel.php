ed889390e2989e56767514200e3f631ed1ea97a2
Halim Teguh Saputro <2131710122@student.polinema.ac.id>
2023-05-23 17:57:26 +0700

    perubahan 6 - memperbaiki CustomerModel

--- app/Models/CustomerModel.php
+++ app/Models/CustomerModel.php
@@ -7,7 +7,5 @@ use CodeIgniter\Model;
 class CustomerModel extends Model
 {
     protected $table = 'customer';
-    protected $primaryKey = 'id_customer';
     protected $allowedFields = ['id_customer', 'nama', 'alamat', 'email', 'tgl_lahhir', 'no_hp', 'jk', 'foto'];
-
 }
